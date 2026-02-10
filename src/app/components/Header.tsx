27d803daca9bb994d70c80ae9176873971b682ff
rontysarkar <rontysarkar2023@gmail.com>
2026-02-11 02:42:48 +0600

    design header for desktop

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,25 +3,43 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { Input } from '@/components/ui/input'
-import { ChevronRight, Lock, Search, User } from 'lucide-react'
+import { toggleLoginDialog } from '@/store/slice/userSlice'
+import { RootState } from '@/store/store'
+import { ChevronRight, Heart, Lock, LogOut, Package, PiggyBank, Search, ShoppingCart, User, User2 } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 
 
 
 const header = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const isLoginOpen = useSelector((state: RootState) => state.user.isLoggedIn)
 
   const user = {
     userPicture: '',
-    name: '',
-    email: '',
+    name: 'Ronty Sarkar',
+    email: 'ronty@gmail.com',
   }
   const userPlaceholder = '';
 
   const handleLogin = () => { }
-  const handleProtectionNavigation = () => { }
+  const handleProtectionNavigation = (href: string) => {
+    if (user) {
+      router.push(href);
+      setIsDropdownOpen(false);
+    } else {
+      dispatch(toggleLoginDialog());
+      setIsDropdownOpen(false);
+    }
+  }
+  const handleLogout = () => { }
 
 
   const menuItems = [
@@ -32,7 +50,7 @@ const header = () => {
           <div className='flex space-x-4 items-center p-2 border-b'>
             <Avatar>
               {user?.userPicture ? (
-                <AvatarImage className='w-10 h-10 rounded-full' src="https://github.com/shadcn.png" alt='user image' />
+                <AvatarImage className='w-12 h-12 rounded-full' src="https://github.com/shadcn.png" alt='user image' />
               ) : (
                 <AvatarFallback className='text-black'>{userPlaceholder}</AvatarFallback>
               )
@@ -51,41 +69,54 @@ const header = () => {
       }
     ] : [
       {
-        icon: <User className='h-5 w-5' />,
-        Lable: "My Profile",
-        onClick: handleProtectionNavigation,
-      }
+        icon: <Lock className='h-5 w-5' />,
+        Lable: "Login/Sign up",
+        onClick: handleLogin,
+      },
     ]),
     {
-      icon: <Lock className='h-5 w-5' />,
-      Lable: "Login/Sign up",
-      onClick: handleLogin,
+      icon: <User className='h-5 w-5' />,
+      Lable: "My Profile",
+      onClick: () => handleProtectionNavigation('/account/profile'),
     },
     {
-      icon: <Lock className='h-5 w-5' />,
+      icon: <Package className='h-5 w-5' />,
       Lable: "My Orders",
-      onClick: handleLogin,
+      onClick: () => handleProtectionNavigation('/account/orders'),
     },
     {
-      icon: <Lock className='h-5 w-5' />,
+      icon: <PiggyBank className='h-5 w-5' />,
       Lable: "My Selling Orders",
-      onClick: handleLogin,
+      onClick: () => handleProtectionNavigation('/account/selling-orders'),
     },
     {
-      icon: <Lock className='h-5 w-5' />,
+      icon: <ShoppingCart className='h-5 w-5' />,
       Lable: "Cart",
-      onClick: handleLogin,
+      onClick: () => handleProtectionNavigation('/checkout/cart'),
     },
     {
-      icon: <Lock className='h-5 w-5' />,
-      Lable: "Wishlist",
-      onClick: handleLogin,
+      icon: <Heart className='h-5 w-5' />,
+      Lable: "My Wishlist",
+      onClick: () => handleProtectionNavigation('/account/wishlist'),
     },
     {
-      icon: <Lock className='h-5 w-5' />,
+      icon: <User2 className='h-5 w-5' />,
       Lable: "About Us",
-      onClick: handleLogin,
+      href: '/about-us'
     },
+    ...(user && user.email ? [
+      {
+        icon: <LogOut className='h-5 w-5' />,
+        Lable: "Logout",
+        onClick: handleLogin,
+      },
+    ] : [
+      {
+        icon: <Lock className='h-5 w-5' />,
+        Lable: "Login/Sign up",
+        onClick: handleLogin,
+      },
+    ])
 
   ]
 
@@ -116,9 +147,15 @@ const header = () => {
   return (
     <header className='border-b bg-white sticky top-0 z-50'>
       {/* desktop header */}
-      <div className='container w-[80%] mx-auto hidden lg:flex items-center justify-between p-4'>
+      <div className='container w-[80%] mx-auto hidden lg:flex items-center justify-around p-4'>
         <Link href='/' className='flex itmes-center'>
-          <h1 className='text-3xl font-semibold'>Bazar <span className='text-red-700'>Point</span></h1>
+          <Image
+            src='/images/web-logo.png'
+            width={450}
+            height={100}
+            alt='desktop-logo'
+            className='h-15 w-auto'
+          />
         </Link>
         <div className='flex flex-1 itmes-center justify-center max-w-xl px-4 '>
           <div className='relative w-full '>
@@ -161,7 +198,17 @@ const header = () => {
               {<MenuItems />}
             </DropdownMenuContent>
           </DropdownMenu>
-
+          <Link href='/checkout/cart'>
+            <div className='relative'>
+              <Button variant='ghost' className='relative' >
+                <ShoppingCart className='w-6 h-6 mr-2' />
+                Cart
+              </Button>
+              {user && (
+                <h1 className='absolute text-xs left-5 top-2 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white rounded-full px-1'>3</h1>
+              )}
+            </div>
+          </Link>
         </div>
       </div>
     </header>
