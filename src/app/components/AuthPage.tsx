20a983afb29991f0e7efa8e75b713fc784f95d58
rontysarkar <rontysarkar2023@gmail.com>
2026-03-12 01:55:56 +0600

    add login modal

--- src/app/components/AuthPage.tsx
+++ src/app/components/AuthPage.tsx
@@ -1,19 +1,56 @@
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Eye, EyeOff, Loader2, Lock, Mail } from "lucide-react";
+import Image from "next/image";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
 interface AuthPageProps {
   isLoginOpen: boolean;
   setIsLoginOpen: (open: boolean) => void;
 }
+interface loginFormData {
+  email: string;
+  password: string;
+}
+interface signUpFormData {
+  name: string;
+  email: string;
+  passwrd: string;
+}
+interface forgotPasswordFormData {
+  email: string;
+}
+
 const AuthPage: React.FC<AuthPageProps> = ({ isLoginOpen, setIsLoginOpen }) => {
   const [currentTab, setCurrentTab] = useState<"login" | "signup" | "forgot">(
     "login",
   );
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginLoading, setLoginLoading] = useState(false);
+  const {
+    register: registerLogin,
+    handleSubmit: handleLoginSubmit,
+    formState: { errors: loginError },
+  } = useForm<loginFormData>();
+  const {
+    register: registerSignUp,
+    handleSubmit: handleSignUpSubmit,
+    formState: { errors: signUpError },
+  } = useForm<signUpFormData>();
+  const {
+    register: registerForgotPassword,
+    handleSubmit: handleForgotPasswordSubmit,
+    formState: { errors: forgotPasswordError },
+  } = useForm<forgotPasswordFormData>();
+
   return (
     <Dialog open={isLoginOpen} onOpenChange={setIsLoginOpen}>
       <DialogContent className="sm:max-w-[425px] p-6">
@@ -21,15 +58,93 @@ const AuthPage: React.FC<AuthPageProps> = ({ isLoginOpen, setIsLoginOpen }) => {
           <DialogTitle className="text-2xl text-center font-bold mb-4">
             Welcome to ID Bazar
           </DialogTitle>
-          <Tabs 
-          value={currentTab}
-          onValueChange={(value)=>setCurrentTab(value as 'login' | 'signup' | 'forgot')}
+          <Tabs
+            value={currentTab}
+            onValueChange={(value) =>
+              setCurrentTab(value as "login" | "signup" | "forgot")
+            }
           >
             <TabsList className="grid w-full grid-cols-3">
-                <TabsTrigger value="login">Login</TabsTrigger>
-                <TabsTrigger value="signup">Sign Up</TabsTrigger>
-                <TabsTrigger value="forgot">Forgot</TabsTrigger>
+              <TabsTrigger value="login">Login</TabsTrigger>
+              <TabsTrigger value="signup">Sign Up</TabsTrigger>
+              <TabsTrigger value="forgot">Forgot</TabsTrigger>
             </TabsList>
+            <TabsContent value="login" className="mt-2 space-y-4">
+              <form className="space-y-4">
+                <div className="relative">
+                  <Input
+                    {...registerLogin("email", {
+                      required: "Email is Required",
+                    })}
+                    placeholder="Email"
+                    type="email"
+                    className="pl-10"
+                  />
+                  <Mail
+                    className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+                    size={20}
+                  />
+                </div>
+                {loginError.email && (
+                  <p className="text-red-500 text-sm">
+                    {loginError.email.message}
+                  </p>
+                )}
+
+                <div className="relative">
+                  <Input
+                    {...registerLogin("password", {
+                      required: "Password is Required",
+                    })}
+                    placeholder="Password"
+                    type={showPassword ? "text" : "password"}
+                    className="pl-10"
+                  />
+                  <Lock
+                    className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+                    size={20}
+                  />
+                  {showPassword ? (
+                    <EyeOff
+                      onClick={() => setShowPassword(false)}
+                      className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+                      size={20}
+                    />
+                  ) : (
+                    <Eye
+                      onClick={() => setShowPassword(true)}
+                      className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+                      size={20}
+                    />
+                  )}
+                </div>
+                {loginError.password && (
+                  <p className="text-red-500 text-sm">
+                    {loginError.password.message}
+                  </p>
+                )}
+                <Button type="submit" className="w-full font-bold">
+                  {loginLoading ? (
+                    <Loader2 className="animate-spin mr-2" size={20}/>
+                  ) :(
+                    "Login"
+                  )}
+                </Button>
+              </form>
+              <div className="flex items-center my-4">
+                <div className="flex-1 h-px bg-gray-300"></div>
+                <p className="mx-2 text-gray-500 text-sm">Or</p>
+                <div className="flex-1 h-px bg-gray-300"></div>
+              </div>
+              <Button className="w-full flex items-center justify-center gap-2 bg-white text-gray-700 border border-gray-300 hover:bg-gray-50">
+                <Image 
+                src="/icons/google.svg"
+                alt="googleIcon"
+                width={20}
+                height={20}
+                />Login with Google
+              </Button>
+            </TabsContent>
           </Tabs>
         </DialogHeader>
       </DialogContent>
